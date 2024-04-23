97583d530490e9e371154bf1202b222baea9d546
healthier-vitamins <cheeaudrian97@gmail.com>
2024-04-24 01:25:43 +0800

    completed selenium script to fill in qr code form

--- src/index.py
+++ src/index.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from dotenv import load_dotenv
 import os
@@ -14,48 +16,91 @@ load_dotenv()
 
 
 def main():
-    name_env = os.getenv("NAME")
-    mobile_env = os.getenv("MOBILE")
+    try:
+        name_env = os.getenv("NAME")
+        mobile_env = os.getenv("MOBILE")
 
-    driver = webdriver.Chrome()
-    driver.get(
-        "https://forms.office.com/pages/responsepage.aspx?id=WGeXB8aT70uz3FOGA9yRbsbr_tTOC29AvupnhyEvx8FUQzNRMkFROE5ZVTRQSU8yQVZEQlg4SDBCSCQlQCN0PWcu"
-    )
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
 
-    name = driver.find_element(
-        By.XPATH, '//*[@id="question-list"]/div[1]/div[2]/div/span/input'
-    )
-    name.clear()
-    name.send_keys(name_env or "")
+        driver = webdriver.Chrome(options=options)
+        driver.get(
+            "https://forms.office.com/pages/responsepage.aspx?id=WGeXB8aT70uz3FOGA9yRbsbr_tTOC29AvupnhyEvx8FUQzNRMkFROE5ZVTRQSU8yQVZEQlg4SDBCSCQlQCN0PWcu"
+        )
 
-    mobile = driver.find_element(
-        By.XPATH, '//*[@id="question-list"]/div[2]/div[2]/div/span/input'
-    )
-    mobile.clear()
-    mobile.send_keys(mobile_env or "")
+        name = driver.find_element(
+            By.XPATH, '//*[@id="question-list"]/div[1]/div[2]/div/span/input'
+        )
+        name.clear()
+        name.send_keys(name_env or "")
 
-    # floor_select = Select(
-    #     driver.find_element(
-    #         By.XPATH, '//*[@id="question-list"]/div[4]/div[2]/div/div/div'
-    #     )
-    # )
-    # floor_select.deselect_all()
-    # floor_select.select_by_visible_text("15")
+        mobile = driver.find_element(
+            By.XPATH, '//*[@id="question-list"]/div[2]/div[2]/div/span/input'
+        )
+        mobile.clear()
+        mobile.send_keys(mobile_env or "")
 
-    purpose = driver.find_element(
-        By.XPATH,
-        '//*[@id="r531850efbc704d2cabb1f836157cd57a_placeholder_content"]/span',
-    )
-    purpose.clear()
-    purpose.send_keys("Meeting")
+        floor = driver.find_element(
+            By.XPATH, '//*[@id="question-list"]/div[4]/div[2]/div/div/div'
+        )
+        floor.click()
 
-    # purpose_select = Select(
-    #     driver.find_element(
-    #         By.XPATH, '//*[@id="question-list"]/div[5]/div[2]/div/div/div'
-    #     )
-    # )
-    # purpose_select.deselect_all()
-    # purpose_select.select_by_visible_text("Meeting")
+        floor_dropdown_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[14]"))
+        )
+
+        if floor_dropdown_element:
+            floor_fifteen_dropdown = driver.find_element(
+                By.XPATH, "/html/body/div[2]/div/div[14]"
+            )
+            floor_fifteen_dropdown.click()
+
+        purpose = driver.find_element(
+            By.XPATH,
+            '//*[@id="question-list"]/div[5]/div[2]/div/div/div',
+        )
+        purpose.click()
+
+        purpose_dropdown_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                # (By.CLASS_NAME, "this-definitely-does-not-exist")
+                (By.XPATH, "/html/body/div[2]/div/div[3]")
+            )
+        )
+
+        if purpose_dropdown_element:
+            purpose_meeting_dropdown = driver.find_element(
+                By.XPATH, "/html/body/div[2]/div/div[3]"
+            )
+            purpose_meeting_dropdown.click()
+
+        submit = driver.find_element(
+            By.XPATH, '//*[@id="form-main-content1"]/div/div/div[2]/div[3]/div/button'
+        )
+        submit.click()
+
+        driver.implicitly_wait(5)
+
+        find_by_xpath = driver.find_element(
+            By.XPATH,
+            '//*[@id="form-main-content1"]/div/div/div[2]/div[1]/div[2]/div[2]/span/text()',
+        )
+        find_by_text = driver.find_element(
+            By.XPATH, "//a[contains(text(), 'You have registered successfully')]"
+        )
+
+        print("find_by_xpath: ", find_by_xpath)
+        print("find_by_text: ", find_by_text)
+
+    except OSError as err:
+        print("OS error:", err)
+    except ValueError:
+        print("Could not convert data to an integer.")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    finally:
+        # driver.quit()
+        print("script finished running")
 
 
 if __name__ == "__main__":
