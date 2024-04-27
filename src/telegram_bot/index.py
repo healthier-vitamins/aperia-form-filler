964a754dd139aab8d1fa8dc6a2972cc8a70b6834
healthy-vitamins <cheeaudrian97@gmail.com>
2024-04-28 04:20:39 +0800

    connected and able to send messages through bot

--- src/telegram_bot/index.py
+++ src/telegram_bot/index.py
@@ -8,12 +8,34 @@ import os
 
 load_dotenv()
 
+import pprint
+import json
+
 
 async def main():
     telegram_env = os.getenv("TELEGRAM_TOKEN")
-    bot = telegram.Bot(telegram_env or '')
+    bot = telegram.Bot(telegram_env or "")
     async with bot:
         print(await bot.get_me())
+        print("====================================================")
+        updates = await bot.get_updates()
+        if updates:
+            for update in updates:
+                print(update)
+                print("====================================================")
+                pprint.pprint(update)
+                print("2 ====================================================")
+                # print(json.dumps(update, indent=4, sort_keys=True))
+                # print("3 ====================================================")
+                if update.message and update.message.from_user:
+                    print(update.message.from_user.id)
+                    print("====================================================")
+                    await bot.send_message(
+                        text="hi", chat_id=update.message.from_user.id
+                    )
+
+        # print(f"updates: {updates}")
+        # print(f"updates[0]: {updates[0]}")
 
 
 if __name__ == "__main__":
